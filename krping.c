1a0f7c9c504c6bc4c19893edb5520f9630897a00
Kim Zhu <zhu.yanjun@ionos.com>
2024-08-04 11:24:37 +0200

    RDMA/krping: Output the cause based on the error number

--- krping.c
+++ krping.c
@@ -205,9 +205,9 @@ static void krping_cq_event_handler(struct ib_cq *cq, void *ctx)
 				DEBUG_LOG("cq flushed\n");
 				continue;
 			} else {
-				printk(KERN_ERR PFX "cq completion failed with "
-				       "wr_id %Lx status %d opcode %d vender_err %x\n",
-					wc.wr_id, wc.status, wc.opcode, wc.vendor_err);
+				pr_err("cq completion failed with "
+				       "wr_id %Lx status: %s opcode %d vender_err %x\n",
+					wc.wr_id, ib_wc_status_msg(wc.status), wc.opcode, wc.vendor_err);
 				goto error;
 			}
 		}
