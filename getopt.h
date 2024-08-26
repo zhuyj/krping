58395af53b7d21bf8568f760c67741f3147f91cd
Zhu Yanjun <yanjun.zhu@linux.dev>
2024-08-26 09:28:29 +0000

    RDMA/krperf: Move instance of options to getopt.h

--- getopt.h
+++ getopt.h
@@ -13,6 +13,31 @@ struct krperf_option {
 	int val;
 };
 
+static const struct krperf_option krperf_opts[] = {
+	{"count", OPT_INT, 'C'},
+	{"size", OPT_INT, 'S'},
+	{"addr", OPT_STRING, 'a'},
+	{"addr6", OPT_STRING, 'A'},
+	{"port", OPT_INT, 'p'},
+	{"verbose", OPT_NOPARAM, 'v'},
+	{"validate", OPT_NOPARAM, 'V'},
+	{"server", OPT_NOPARAM, 's'},
+	{"client", OPT_NOPARAM, 'c'},
+	{"server_inv", OPT_NOPARAM, 'I'},
+	{"wlat", OPT_NOPARAM, 'l'},
+	{"rlat", OPT_NOPARAM, 'L'},
+	{"bw", OPT_NOPARAM, 'B'},
+	{"duplex", OPT_NOPARAM, 'd'},
+	{"tos", OPT_INT, 't'},
+	{"txdepth", OPT_INT, 'T'},
+	{"poll", OPT_NOPARAM, 'P'},
+	{"local_dma_lkey", OPT_NOPARAM, 'Z'},
+	{"read_inv", OPT_NOPARAM, 'R'},
+	{"fr", OPT_NOPARAM, 'f'},
+	{"srq", OPT_NOPARAM, 'q'},
+	{NULL, 0, 0}
+};
+
 extern int krperf_getopt(const char *caller, char **options, const struct krperf_option *opts,
 		      char **optopt, char **optarg, unsigned long *value);
 
--- krperf.h
+++ krperf.h
@@ -6,31 +6,6 @@
 #ifndef _KRPERF_H
 #define _KRPERF_H
 
-static const struct krperf_option krperf_opts[] = {
-	{"count", OPT_INT, 'C'},
-	{"size", OPT_INT, 'S'},
-	{"addr", OPT_STRING, 'a'},
-	{"addr6", OPT_STRING, 'A'},
-	{"port", OPT_INT, 'p'},
-	{"verbose", OPT_NOPARAM, 'v'},
-	{"validate", OPT_NOPARAM, 'V'},
-	{"server", OPT_NOPARAM, 's'},
-	{"client", OPT_NOPARAM, 'c'},
-	{"server_inv", OPT_NOPARAM, 'I'},
- 	{"wlat", OPT_NOPARAM, 'l'},
- 	{"rlat", OPT_NOPARAM, 'L'},
- 	{"bw", OPT_NOPARAM, 'B'},
- 	{"duplex", OPT_NOPARAM, 'd'},
-	{"tos", OPT_INT, 't'},
- 	{"txdepth", OPT_INT, 'T'},
- 	{"poll", OPT_NOPARAM, 'P'},
- 	{"local_dma_lkey", OPT_NOPARAM, 'Z'},
- 	{"read_inv", OPT_NOPARAM, 'R'},
- 	{"fr", OPT_NOPARAM, 'f'},
-	{"srq", OPT_NOPARAM, 'q'},
-	{NULL, 0, 0}
-};
-
 struct krperf_stats {
 	unsigned long long send_bytes;
 	unsigned long long send_msgs;
